8383a8294257cfb960a8c699f0d1fa84647eaac7
Spencer Christy <spenrad@gmail.com>
2021-01-19 20:20:02 -0800

    added prompt questions

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 // TODO: Include packages needed for this application
+const marky = require('./utils/generateMarkdown.js');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+      },
+
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your e-mail address?'
+      },
+      {
+        type: 'input',
+        name: 'project name',
+        message: "What is your project's name?"
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Breifly describe your project...'
+      },
+      {
+        type: 'list',
+        name: 'liscence',
+        message: 'Which license does your project have?',
+        choices: ['MIT', 'IBM', 'ISC', 'Mozilla']
+      },
+      {
+        type: 'input',
+        name: 'dependencies',
+        message: 'What command should be run to install dependencies?'
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run to run tests?'
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What technologies should the user be familiar with if using this repo?'
+      },
+      {
+        type: 'input',
+        name: 'contribute',
+        messsge: 'What technologies should the user be familiar with to contribute to the repo?'
+      }
+
+    ])
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
